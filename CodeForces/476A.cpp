a7832408b1eed83bcf19064a3b5a62eb19cae3eb
shreyaasridhar <shreyanju@gmail.com>
2017-01-28 00:15:11 +0530

    idk SOme changes

--- CodeForces/476A.cpp
+++ CodeForces/476A.cpp
@@ -8,8 +8,8 @@ int main() {
 	if(n<m){
 		cout<<-1;return 0;
 	}
-	s=(n/2)%m;
-	if(s==0) cout<<n/2;
+	int s=(n/2)%m;
+	if(s==0 && n%2==0) cout<<n/2;
 	else cout<<((n/2)+m-s);
 	return 0;
 }	
--- CodeForces/490A.cpp
+++ CodeForces/490A.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
 using namespace std;
 
 int main() {
-	int n;
+	int n,ele;
 	cin>>n;
-	int ele[n];
 	vector<int> a,b,c; 
 	for(int i=0;i<n;++i){
-		cin>>ele[i];
-		if(ele[i]==1) a.push_back(i);
-		else if(ele[i]==2) b.push_back(i);
-		else if(ele[i]==3) c.push_back(i);
+		cin>>ele;
+		if(ele==1) a.push_back(i);
+		else if(ele==2) b.push_back(i);
+		else if(ele==3) c.push_back(i);
 	}
 	int m=min(a.size(),min(b.size(),c.size() ) );
 	if(m==0){cout<<0; return 0;}
@@ -19,7 +19,6 @@ int main() {
 	for(int i=0;i<m;i++){
 		cout<<a[i]+1<<" "<<b[i]+1<<" "<<c[i]+1<<endl;
 	}
-	
 	
 	return 0;
 }
